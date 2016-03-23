87c28bee344e0c32177791a11f6e57ef81613fef
markzyxelle <markzyxelle@gmail.com>
2016-03-24 00:20:15 +0800

    fixed

--- database/migrations/2016_03_17_054656_create_clients_table.php
+++ database/migrations/2016_03_17_054656_create_clients_table.php
@@ -14,8 +14,6 @@ class CreateClientsTable extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->bigIncrements('id');
-<<<<<<< HEAD
-<<<<<<< HEAD
             $table->bigInteger('barangay_id');            //edit
             $table->bigInteger('group_id');                 //edit
             $table->integer('status_id')->unsigned();
@@ -23,14 +21,8 @@ class CreateClientsTable extends Migration
             $table->integer('civil_status_id')->unsigned();     //edit
             $table->integer('beneficiary_type_id')->unsigned(); //edit
             $table->integer('birthplace')->unsigned();  //edit
-=======
             $table->bigInteger('barangay_code')->unsigned();
             $table->bigInteger('group_id')->unsigned();
->>>>>>> parent of ef9caf6... working approval of pending users
-=======
-            $table->bigInteger('barangay_code');            //edit
-            $table->bigInteger('group_id');                 //edit
->>>>>>> parent of 33c5c37... fixed administrator approval and added roles page
             $table->string('personal_id')->nullable();
             $table->string('national_id')->nullable();
             $table->string('first_name');
@@ -39,23 +31,10 @@ class CreateClientsTable extends Migration
             $table->string('suffix', 10)->nullable();
             $table->string('mother_middle_name')->nullable();
             $table->date('birthdate');
-            $table->string('birthplace');
             $table->string('house_number');
             $table->string('street');
-<<<<<<< HEAD
-<<<<<<< HEAD
             $table->string('mobile_number', 15);
             $table->boolean('isDummy');
-=======
-            $table->enum('gender', ['male', 'female']);
-            $table->enum('civil_status', ['single', 'married', 'separated', 'widowed']);
-            $table->integer('mobile_number', 15);
->>>>>>> parent of ef9caf6... working approval of pending users
-=======
-            $table->enum('gender', ['male', 'female']);
-            $table->enum('civil_status', ['single', 'married', 'separated', 'widowed']);
-            $table->string('mobile_number', 15);
->>>>>>> parent of 33c5c37... fixed administrator approval and added roles page
             $table->timestamps();
         });
     }
